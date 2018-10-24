f97578a8e4285395da6613f99428efb7204fcb3c
itsolution-git <roy.smith0820@gmail.com>
2018-10-24 18:34:35 +0700

    fix issues

--- Denning/Scenes/Search/FileUpload/FileNameAutoComplete.m
+++ Denning/Scenes/Search/FileUpload/FileNameAutoComplete.m
@@ -143,6 +143,10 @@
  possibleCompletionsForString:(NSString *)string
             completionHandler:(void (^)(NSArray *))handler
 {
+    if ([DataManager sharedManager].isSessionExpired == YES) {
+        return;
+    }
+    
     if ([NSOperationQueue mainQueue].operationCount > 0) {
         [[NSOperationQueue mainQueue] cancelAllOperations];
     }
@@ -163,6 +167,7 @@
                                                                           NSLog(@"%@", error);
                                                                           if (((NSHTTPURLResponse *)task.response).statusCode == 408) { // Session expired.
                                                                               [QMAlert showAlertWithMessage:NSLocalizedString(@"STR_SESSION_EXPIRED", nil) actionSuccess:NO inViewController:self];
+                                                                               [DataManager sharedManager].isSessionExpired = YES; 
                                                                           }
                                                                       }];
     [[NSOperationQueue mainQueue] addOperation:operation];
